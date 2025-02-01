535e7bd89f1c807537d92c4836a2c7f1c8e4f08f
Aaron Ramos <aaron.rm.hr@gmail.com>
2025-02-01 16:29:35 -0600

    variable friendsList fixed and add evaluation for check existance's name

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let firendsList = [];
+let friendsList = [];
 
 // Funcion para agregar amigos
 function addFriend(){
    // Se obtiene el valor del input con el id: 'amigo', y se guarda en la variable addNameFriend
-   let addNameFriend = document.getElementById('amigo').value;
+   let nameFriend = document.getElementById('amigo').value;
    cleanInputBox();
-   console.log(addNameFriend);
+
+   // Se evalua si el nombre ya existe en la lista de amigos
+   if(friendsList.includes(nameFriend)){
+      // En caso de existir el nombre se lanza alerta
+      alert("Atencion! Ya has ingresado este nombre");
+   }  else {
+      // Caso contrario: se agrega a la lista
+      friendsList.push(nameFriend);
+   }
    return;
 }
-
 
 // Funcion para limpiar la caja de texto
 function cleanInputBox(){
