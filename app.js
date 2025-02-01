a5319fdb15805788276e79bb227f167d0288464c
Aaron Ramos <aaron.rm.hr@gmail.com>
2025-02-01 16:13:55 -0600

    Add array for friends and addFriends function in app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let firendsList = [];
+
+// Funcion para agregar amigos
+function addFriend(){
+   // Se obtiene el valor del input con el id: 'amigo', y se guarda en la variable addNameFriend
+   let addNameFriend = document.getElementById('amigo').value;
+   cleanInputBox();
+   console.log(addNameFriend);
+   return;
+}
+
+
+// Funcion para limpiar la caja de texto
+function cleanInputBox(){
+   document.getElementById('amigo').value = '';
+}
