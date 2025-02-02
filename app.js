73d9736dfdc9e03a4874375a496cd32017251736
Aaron Ramos <aaron.rm.hr@gmail.com>
2025-02-01 21:14:21 -0600

    Add function to add on the screen each friend added by user

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let friendsList = [];
+let nameFriend = "";
+const friendsList = [];
+const showedList = document.getElementById('listaAmigos');
 
 // Funcion para agregar amigos
 function addFriend(){
    // Se obtiene el valor del input con el id: 'amigo', y se guarda en la variable addNameFriend
-   let nameFriend = document.getElementById('amigo').value;
+   nameFriend = document.getElementById('amigo').value;
    cleanInputBox();
 
    // Se evalua si el nombre ya existe en la lista de amigos
@@ -16,8 +18,23 @@ function addFriend(){
       // Caso contrario: se agrega a la lista
       friendsList.push(nameFriend);
    }
+   console.log(friendsList);
    return;
 }
+
+// Funcion para actualizar la lista de amigos en el DOM
+function updateFriendsList() {
+   // Limpia la lista, para evitar valores dubplicados
+   showedList.innerHTML = "";
+
+   // Bucle para imprimir cada nombre registrado, agregando un elemento <li>
+   for (let i = 0; i < friendsList.length; i++){
+      const friend = document.createElement('li');
+      friend.innerHTML = `${friendsList[i]}`;
+      showedList.appendChild(friend);
+   }
+}
+
 
 // Funcion para limpiar la caja de texto
 function cleanInputBox(){
