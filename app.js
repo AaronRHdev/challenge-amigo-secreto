c6d07fdd6fbfc0c7ce75a9212e1575a986552c4d
Aaron Ramos <aaron.rm.hr@gmail.com>
2025-02-02 18:34:05 -0600

    Fixed funcionallity, names are now added correctly and the draw works correctly

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+let nameFriend = "";
 const friendsList = [];
 const showedList = document.getElementById('listaAmigos');
 
 // Funcion para agregar amigos
 function addFriend(){
    // Se obtiene el valor del input con el id: 'amigo', y se guarda en la variable addNameFriend
-   let nameFriend = document.getElementById('amigo').value;
-   cleanInputBox();
-
-   if (nameFriend = document.getElementById('amigo').value === ''){
+   nameFriend = document.getElementById('amigo').value;
+   if (nameFriend === ''){
       alert("Por favor ingresa un nombre");
    } else {
+      cleanInputBox();
       // Se evalua si el nombre ya existe en la lista de amigos
       if(friendsList.includes(nameFriend)){
          // En caso de existir el nombre se lanza alerta
          alert("Atencion! Ya has ingresado este nombre");
       }  else {
-       // Caso contrario: se agrega a la lista
+         // Caso contrario: se agrega a la lista
          friendsList.push(nameFriend);
       }
    }
-   
    return;
 }
 
